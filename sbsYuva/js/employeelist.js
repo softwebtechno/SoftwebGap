8a4a58a868915fbd5d08fb53a738da70ae026830
softwebtechno <umesh.v@keyamart.com>
2015-02-07 15:33:16 +0530

    My Yuva
    this is a first yuva Apps

---
+++ sbsYuva/js/employeelist.js
@@ -0,0 +1,32 @@
+var serviceURL = "http://localhost:8080/services/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+
+$('#employeeListPage').live('pageshow', function(event) {
+	getEmployeeList();
+});
+
+document.addEventListener("CloseMe", function(e){
+    
+        navigator.app.exitApp();
+        
+}, false);
+
+function getEmployeeList() {
+	$.getJSON(serviceURL + 'getemployees.php', function(data) {
+		$('#employeeList li').remove();
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+					'<img src="pics/' + employee.picture + '"/>' +
+					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+					'<p>' + employee.title + '</p>' +
+					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+		});
+		$('#employeeList').listview('refresh');
+	});
+}
